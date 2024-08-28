b5426ba7440341e05d548d1b216250488fc8e804
akhil kumar <akmusham@gmail.com>
2024-08-28 13:24:57 -0400

    list of cards design with hover shadow

--- src/Components/campaign.jsx
+++ src/Components/campaign.jsx
@@ -1,12 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import './index.css'
+import React, { useEffect, useState } from "react";
+import "./index.css";
 
-function Campaign({campaign, handleOnCampaign}) {
-
-  return <div className='campaign-wrapper' onClick={() => handleOnCampaign(campaign.id)}>
-              <span>{campaign.id + 1}</span>
-              <span className='campaign-name'>{campaign.name}</span>
-          </div>
+function Campaign({ campaign, handleOnCampaign }) {
+  return (
+    <div
+      className="campaign-wrapper"
+      onClick={() => handleOnCampaign(campaign.id)}
+    >
+      <span>{campaign.id + 1}</span>
+      <span className="campaign-name">{campaign.name}</span>
+      <span className="campaign-name">
+        {new Date(campaign.date).toDateString()}
+      </span>
+      <span className="campaign-name">{campaign.description}</span>
+    </div>
+  );
 }
 
 export default Campaign;
--- src/mocks/campaign-list.js
+++ src/mocks/campaign-list.js
@@ -1,10 +1,16 @@
-export function getCampaigns(){
-    return [
-        { id: 0, name: 'Red' },
-        { id: 1, name: 'Green' },
-        { id: 2, name: 'Blue' },
-        { id: 3, name: 'Yellow' },
-        { id: 4, name: 'Purple' },
-        { id: 5, name: 'Orange' },
-    ];
+export function getCampaigns() {
+  let campaigns = [
+    { id: 0, name: "Red" },
+    { id: 1, name: "Green" },
+    { id: 2, name: "Blue" },
+    { id: 3, name: "Yellow" },
+    { id: 4, name: "Purple" },
+    { id: 5, name: "Orange" },
+  ];
+  return campaigns.map((campaign) => ({
+    ...campaign,
+    date: new Date().toISOString(),
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dignissim ullamcorper felis, id vehicula sem fringilla vitae. Pellentesque feugiat volutpat neque.",
+  }));
 }
