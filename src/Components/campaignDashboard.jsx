f93607e9a606d6d3b80931300d12dd8243d9f4f6
akhil kumar <akmusham@gmail.com>
2024-08-28 12:19:56 -0400

    pinging endpoint every 5 sec

--- src/Components/campaignDashboard.jsx
+++ src/Components/campaignDashboard.jsx
@@ -2,14 +2,19 @@ import React, { useEffect, useState } from 'react';
 import './index.css'
 
 function CampaignDashboard({SelectedCampaign, setCampaign}) {
-    const [campaignDetails, setCampaignDetails] = useState(null);
+    const [campaignDetails, setCampaignDetails] = useState({});
+    const [counter, setCounter] = useState(0);
+    const interval = 5000 // to invoke endpoint every 5 sec
 
     useEffect(()=>{
-        fetchCampaignDetails()
+        setInterval(() => {
+          fetchCampaignDetails()
+          setCounter(counter + 1)
+        }, interval);
     },[SelectedCampaign])
 
     const fetchCampaignDetails = () => {
-        fetch(`/api/campaigns/${SelectedCampaign.id}`)
+        fetch(`/api/campaigns/${SelectedCampaign.id}?number=${counter}`)
         .then((response) => response.json())
         .then((data) => setCampaignDetails(data));
       }
